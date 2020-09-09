39b83fde413af0b31bdec2772d56bfbfd5e600d4
romeo611199 <romeo611199@gmail.com>
2020-09-09 22:26:40 +0530

    Adding new component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import Menu from './components/MenuComponent';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
