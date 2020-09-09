cd1fa6e1d21c2d18b942ade7b183a94e10faa9c4
romeo611199 <romeo611199@gmail.com>
2020-09-09 20:57:48 +0530

    React configuring - Bootstrap

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+        </div>
+      </Navbar>
+    </div>
+  );
+}
+
+export default App;
