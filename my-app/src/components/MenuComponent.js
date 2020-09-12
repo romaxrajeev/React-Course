f0846df635afb52d6446d6e00cb8e10f0a01a491
romeo611199 <romeo611199@gmail.com>
2020-09-12 21:56:50 +0530

    Header and Footer

--- my-app/src/components/MenuComponent.js
+++ my-app/src/components/MenuComponent.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import DishDetailComponent from './DishDetailComponent';
 
 class Menu extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+          selectedDish: null
+        } 
+    }
+
+    onDishSelect(dish){
+      this.setState({selectedDish : dish});
+    }
 
     render() {
         console.log(this.props.dishes);
         const menu = this.props.dishes.map((dish) => {
             return (
               <div key={dish.id} className="col-12 col-md-5 m-1">
-                <Card key={dish.id} onClick={() => this.props.onClick(dish.id)}>
-                  
+                
+                <Card onClick={() => this.onDishSelect(dish)}>
                 <CardImg width="100%" src={dish.image} alt={dish.name} />  
-                  
                   <CardImgOverlay>
                     <CardTitle>{dish.name}</CardTitle>
                   </CardImgOverlay>
                 </Card>
+                
               </div>
             );
         });
@@ -25,6 +37,7 @@ class Menu extends Component {
             <div className="row">
                   {menu}
             </div>
+              <DishDetailComponent selectedDish={this.state.selectedDish} />
           </div>
         );
     }
