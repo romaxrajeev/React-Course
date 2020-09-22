2dd149f393a01ff6bab8494699030c6684ba2843
romeo611199 <romeo611199@gmail.com>
2020-09-22 21:54:31 +0530

    Combining Reducers

--- my-app/src/redux/configureStore.js
+++ my-app/src/redux/configureStore.js
@@ -1,11 +1,19 @@
 import { Redirect } from 'react-router-dom';
-import { createStore } from 'redux';
-import { Reducer, initialState } from './reducer';
+import { createStore, combineReducers } from 'redux';
+import { Dishes } from './dishes';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
+import  { Leaders } from './leaders';
 
 export const ConfigureStore = () => {
+    //Map each state to each simpler reducer
     const store = createStore(
-        Reducer,
-        initialState
+        combineReducers({
+            dishes: Dishes,
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders
+        })
     );
 
     return store;
